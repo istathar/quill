18beffd662c1f5a011c6a3db600630110b9c118d
Andrew Cowie <andrew@operationaldynamics.com>
2008-11-24 00:40:53 +1100

    Preliminary support to splice a Chunk into an existing Text stream

--- src/com/operationaldynamics/textbase/Text.java
+++ src/com/operationaldynamics/textbase/Text.java
@@ -21,6 +21,11 @@ public class Text
         chunks[0] = new Chunk(str);
     }
 
+    Text(Chunk initial) {
+        chunks = new Chunk[1];
+        chunks[0] = initial;
+    }
+
     public String toString() {
         StringBuilder str;
 
@@ -33,17 +38,62 @@ public class Text
         return str.toString();
     }
 
-    public void append(Chunk chunk) {
+    public void append(Chunk addition) {
         final Chunk[] next;
         final int i;
 
-        if (chunk == null) {
+        if (addition == null) {
             throw new IllegalArgumentException();
         }
 
         i = chunks.length;
         next = Arrays.copyOf(chunks, i + 1);
-        next[i] = chunk;
+        next[i] = addition;
+
+        chunks = next;
+    }
+
+    public void splice(Chunk which, int point, Chunk addition) {
+        final Chunk before, after;
+        final Chunk[] next;
+        int i, j;
+
+        before = new Chunk(which, 0, point);
+        after = new Chunk(which, point, which.length - point);
+
+        next = new Chunk[chunks.length + 2];
+
+        /*
+         * Identify the index which corresponds to. Yes, a List might be
+         * better here.
+         */
+
+        for (i = 0; i < chunks.length; i++) {
+            if (chunks[i] == which) {
+                break;
+            }
+        }
+
+        if (i != 0) {
+            System.arraycopy(chunks, 0, next, 0, i);
+        }
+
+        /*
+         * Now replace which with before, addition, after
+         */
+        j = i;
+
+        next[j++] = before;
+        next[j++] = addition;
+        next[j++] = after;
+
+        /*
+         * And not copy the remainder of the original array, if any.
+         */
+        i++;
+        if (i != chunks.length) {
+            System.arraycopy(chunks, i, next, j, chunks.length - i);
+        }
 
         chunks = next;
     }
--- tests/com/operationaldynamics/textbase/ValidateText.java
+++ tests/com/operationaldynamics/textbase/ValidateText.java
@@ -55,4 +55,51 @@ public class ValidateText extends TestCase
 
         assertEquals("system broadcast Emergency", text.toString());
     }
+
+    public final void testInitialSplice() {
+        final Text text;
+        final Chunk initial, addition;
+
+        initial = new Chunk("Cononate");
+        addition = new Chunk("cat");
+
+        text = new Text(initial);
+        assertEquals("Cononate", text.toString());
+        assertEquals(1, text.chunks.length);
+
+        text.splice(initial, 3, addition);
+        assertEquals(3, text.chunks.length);
+        assertEquals("Concatonate", text.toString());
+    }
+
+    public final void testMultipleSplice() {
+        final Text text;
+        final Chunk one, two, three, four, space, addition;
+
+        one = new Chunk("One");
+        space = new Chunk(" ");
+        two = new Chunk("Two");
+        three = new Chunk("Three");
+        four = new Chunk("Four");
+
+        text = new Text(one);
+        text.append(space);
+        text.append(two);
+        text.append(space);
+        text.append(three);
+        text.append(space);
+        text.append(four);
+
+        assertEquals("One Two Three Four", text.toString());
+        assertEquals(7, text.chunks.length);
+
+        /*
+         * Now, try splicing something in
+         */
+
+        addition = new Chunk("wentyT");
+        text.splice(two, 1, addition);
+        assertEquals("One TwentyTwo Three Four", text.toString());
+        assertEquals(9, text.chunks.length);
+    }
 }
