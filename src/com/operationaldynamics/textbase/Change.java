caada5e696afea0749e1690bc9c28c4d6710b8e5
Andrew Cowie <andrew@operationaldynamics.com>
2008-11-28 16:16:17 +1100

    Stub for a Change based infrastructure

---
+++ src/com/operationaldynamics/textbase/Change.java
@@ -0,0 +1,27 @@
+/*
+ * Change.java
+ *
+ * Copyright (c) 2008 Operational Dynamics Consulting Pty Ltd
+ * 
+ * The code in this file, and the program it is a part of, are made available
+ * to you by its authors under the terms of the "GNU General Public Licence,
+ * version 2" See the LICENCE file for the terms governing usage and
+ * redistribution.
+ */
+package com.operationaldynamics.textbase;
+
+public class Change
+{
+
+}
+
+class LoadFile extends Change
+{
+    /**
+     * Load a String into a Text.
+     */
+    LoadFile(String text) {
+
+    }
+
+}
