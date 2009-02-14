edd991df6ac891099cac603fd586bd7192338562
Andrew Cowie <andrew@operationaldynamics.com>
2009-02-15 01:20:32 +1100

    Test inline formatting in conversion
    Write a unit test ensuring that complex (nested inline Common Markup)
    formatting is properly converted to DocBook.
    
    Will need correction when we change the output wrap width.

--- tests/markerpen/converter/ValidateStackToDocBookConversion.java
+++ tests/markerpen/converter/ValidateStackToDocBookConversion.java
@@ -17,6 +17,8 @@ import java.io.IOException;
 import junit.framework.TestCase;
 import markerpen.docbook.Document;
 import markerpen.textbase.Change;
+import markerpen.textbase.CharacterSpan;
+import markerpen.textbase.Common;
 import markerpen.textbase.InsertChange;
 import markerpen.textbase.Span;
 import markerpen.textbase.StringSpan;
@@ -102,4 +104,86 @@ public class ValidateStackToDocBookConversion extends TestCase
 
         return buf.toString();
     }
+
+    public final void testWriteComplexPara() throws IOException {
+        final TextStack stack;
+        final Span[] spans;
+        int offset;
+        Change change;
+        final DocBookConverter converter;
+        final Document book;
+        final ByteArrayOutputStream out;
+        final String blob;
+
+        /*
+         * Build up a more complicated example
+         */
+
+        spans = new Span[] {
+                new StringSpan("Accessing the ", null),
+                new StringSpan("/tmp", Common.FILENAME),
+                new StringSpan(" directory directly is fine, but the ", null),
+                new StringSpan("File", Common.TYPE),
+                new StringSpan(" class ", null),
+                new CharacterSpan('h', null),
+                new CharacterSpan('a', null),
+                new CharacterSpan('s', null),
+                new CharacterSpan(' ', null),
+                new CharacterSpan('a', null),
+                new CharacterSpan(' ', null),
+                new StringSpan("createTemp", Common.FUNCTION),
+                new CharacterSpan('F', Common.FUNCTION),
+                new CharacterSpan('i', Common.FUNCTION),
+                new CharacterSpan('l', Common.FUNCTION),
+                new CharacterSpan('e', Common.FUNCTION),
+                new StringSpan("()", Common.FUNCTION),
+                new StringSpan(" function that you are often better off using.", null),
+        };
+
+        stack = new TextStack();
+        offset = 0;
+
+        for (Span span : spans) {
+            change = new InsertChange(offset, span);
+            stack.apply(change);
+            offset += span.getWidth();
+        }
+
+        /*
+         * Now run conversion process.
+         */
+
+        converter = new DocBookConverter();
+        converter.append(stack);
+        book = converter.result();
+
+        assertNotNull(book);
+
+        out = new ByteArrayOutputStream();
+        book.toXML(out);
+
+        blob = combine(new String[] {
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+                "<book version=\"5.0\" xmlns=\"http://docbook.org/ns/docbook\">",
+                "<chapter>",
+                "<section>",
+                "<para>",
+                "Accessing the " + "<filename>/tmp</filename>" + " directory",
+                "directly is fine, but the " + "<type>" + "File" + "</type>",
+                "class has a " + "<function>" + "createTempFile()" + "</function>",
+                "function that you are often better off using.",
+                "</para>",
+                "</section>",
+                "</chapter>",
+                "</book>"
+        });
+
+        /*
+         * WARNING. If the word wrap width of our save output changes, then
+         * the expected result blob will need to be reformatted to for the
+         * test to pass. That's acceptable.
+         */
+        assertEquals(blob, out.toString());
+    }
+
 }
