2db53bf5ba71f324693ce01fdce82fe19920d9da
Andrew Cowie <andrew@operationaldynamics.com>
2009-03-14 22:57:41 +1100

    Create Series as a wrapper around an array of Segments

---
+++ src/quill/textbase/Series.java
@@ -0,0 +1,34 @@
+/*
+ * Series.java
+ *
+ * Copyright (c) 2009 Operational Dynamics Consulting Pty Ltd
+ * 
+ * The code in this file, and the program it is a part of, are made available
+ * to you by its authors under the terms of the "GNU General Public Licence,
+ * version 2" See the LICENCE file for the terms governing usage and
+ * redistribution.
+ */
+package quill.textbase;
+
+/**
+ * A collection of Components, comprising a visible section of a document.
+ * Like other areas in textbase, is is a wrapper around an array.
+ * 
+ * @author Andrew Cowie
+ */
+public class Series
+{
+    private final Segment[] segments;
+
+    Series(Segment[] segments) {
+        this.segments = segments;
+    }
+
+    public int size() {
+        return segments.length;
+    }
+
+    public Segment get(int index) {
+        return segments[index];
+    }
+}
