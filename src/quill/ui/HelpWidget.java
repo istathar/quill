4a8048db135edf9172987dbfa6f42084ebad2676
Andrew Cowie <andrew@operationaldynamics.com>
2009-03-15 23:31:12 +1100

    Remove elements from help view
    Add a placeholder section about actions (save, print).

--- src/quill/ui/HelpWidget.java
+++ src/quill/ui/HelpWidget.java
@@ -40,60 +40,60 @@ class HelpWidget extends VBox
 
     HelpWidget() {
         super(false, 0);
-        final String[][] views, markup;
+        final String[][] views, actions, markup;
         vbox = this;
 
         views = new String[][] {
                 new String[] {
-                        "F1", "", "Help", "This help screen"
+                        "F1", "Help", "This help screen"
                 },
                 new String[] {
                         "F2",
-                        "",
                         "Preview",
                         "A preview of what the current page will look like in its final rendered form."
                 },
                 new String[] {
                         "F3",
-                        "",
                         "Outline",
                         "An outline of the documents chapters and section headings allowing you to navigate."
                 },
                 new String[] {
                         "F11",
-                        "",
                         "Fullscreen",
                         "Work in fullscreen mode, giving you an even better view of your document."
                 }
         };
 
+        actions = new String[][] {
+                new String[] {
+                        "Ctrl+S", "Save", "Save the document."
+                }, new String[] {
+                        "Ctrl+P", "Print", "Render the document to PDF so you can print it."
+                },
+        };
+
         markup = new String[][] {
                 new String[] {
                         "Ctrl+Shift+Space",
-                        "",
                         "Clear!",
                         "Remove any inline formatting from the currently selected range."
                 },
                 new String[] {
-                        "Ctrl+I", "emphasis", "Italics", "Make the text appear in italics."
+                        "Ctrl+I", "Italics", "Make the text appear in italics."
                 },
                 new String[] {
-                        "Ctrl+B", "emphasis role=\"bold\"", "Bold", "Make the text appear in bold face."
+                        "Ctrl+B", "Bold", "Make the text appear in bold face."
                 },
                 new String[] {
-                        "Ctrl+Shift+F", "filename", "File", "A path on the filesystem."
+                        "Ctrl+Shift+F", "File", "A path on the filesystem."
                 },
                 new String[] {
                         "Ctrl+Shift+T",
-                        "type",
                         "Class or Type",
                         "The name of an object-oriented class or similar type."
                 },
                 new String[] {
-                        "Ctrl+Shift+M",
-                        "function",
-                        "Method or Function",
-                        "The name of a method or function call."
+                        "Ctrl+Shift+M", "Method or Function", "The name of a method or function call."
                 },
         };
 
@@ -101,12 +101,17 @@ class HelpWidget extends VBox
 
         addHeading("View selection");
         for (String[] line : views) {
-            addHelpLine(line[0], line[2], line[3], line[1]);
+            addHelpLine(line[0], line[1], line[2]);
+        }
+
+        addHeading("Actions");
+        for (String[] line : actions) {
+            addHelpLine(line[0], line[1], line[2]);
         }
 
         addHeading("Markup keys");
         for (String[] line : markup) {
-            addHelpLine(line[0], line[2], line[3], line[1]);
+            addHelpLine(line[0], line[1], line[2]);
         }
     }
 
@@ -122,7 +127,7 @@ class HelpWidget extends VBox
         vbox.packStart(heading, false, false, 6);
     }
 
-    private void addHelpLine(String keys, String title, String description, String element) {
+    private void addHelpLine(String keys, String title, String description) {
         Label label;
         HBox spread;
         StringBuilder str;
@@ -139,12 +144,6 @@ class HelpWidget extends VBox
         str.append(title);
         str.append("</b>\n");
         str.append(description);
-
-        if (!element.equals("")) {
-            str.append("\n<span font='Deja Vu Sans Mono'>&lt;");
-            str.append(element);
-            str.append("&gt;</span>");
-        }
 
         label = new Label();
         label.setLabel(str.toString());
