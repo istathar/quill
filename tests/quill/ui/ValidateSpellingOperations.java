90753d59dc8659edb69ef00b79e8de8c83cf3e24
Andrew Cowie <andrew@operationaldynamics.com>
2009-10-31 12:14:59 +1100

    Test spelling of text with various Spans

--- tests/quill/ui/ValidateSpellingOperations.java
+++ tests/quill/ui/ValidateSpellingOperations.java
@@ -20,6 +20,7 @@ import org.gnome.gtk.TextIter;
 import org.gnome.gtk.TextTag;
 
 import quill.textbase.Change;
+import quill.textbase.Common;
 import quill.textbase.DataLayer;
 import quill.textbase.Folio;
 import quill.textbase.InsertTextualChange;
@@ -140,6 +141,127 @@ public class ValidateSpellingOperations extends GraphicalTestCase
             assertEquals(0, tags.length);
             pointer.forwardChar();
         }
+        assertTrue(pointer.isEnd());
+    }
+
+    public final void testNotSpellingBasedOnMarkup() throws ValidityException, ParsingException,
+            IOException {
+        final DataLayer data;
+        final Folio folio;
+        final Span[] spans;
+        final Segment segment;
+        final TextChain chain;
+        Change change;
+        Span span;
+        final EditorTextView editor;
+        final TextBuffer buffer;
+        TextIter start, finish, pointer;
+        String word;
+        int i, j;
+        TextTag[] tags;
+        TextTag tag;
+
+        data = new DataLayer();
+        ui = new UserInterface(data);
+
+        data.createDocument();
+        folio = data.getActiveDocument();
+        ui.displayDocument(folio);
+
+        /*
+         * Establish some starting text.
+         */
+
+        spans = new Span[] {
+                createSpan("Use ", null),
+                createSpan("dsmthng()", Common.FUNCTION),
+                createSpan(" speek.", null),
+        };
+
+        segment = folio.get(0).get(1);
+        chain = segment.getText();
+
+        for (i = 0, j = 0; i < spans.length; i++) {
+            span = spans[i];
+            change = new InsertTextualChange(chain, j, span);
+            ui.apply(change);
+            j += span.getWidth();
+        }
+
+        assertEquals("Use dsmthng() speek.", chain.toString());
+        assertEquals(20, chain.length());
+
+        /*
+         * Verify that the thing we think is a spelling mistake actually is.
+         */
+
+        assertFalse(ui.dict.check("dsmthng"));
+
+        /*
+         * Initial state ok. Good. Now get a reference to the TextView, and
+         * see what it has done.
+         */
+
+        editor = (EditorTextView) findEditor(ui.primary.getChild());
+        buffer = editor.getBuffer();
+        pointer = buffer.getIterStart();
+
+        start = buffer.getIterStart();
+        finish = buffer.getIter(3);
+
+        word = buffer.getText(start, finish, true);
+        assertEquals("Use", word);
+
+        pointer = buffer.getIterStart();
+
+        for (i = 0; i < 4; i++) {
+            tags = pointer.getTags();
+            assertEquals(0, tags.length);
+            pointer.forwardChar();
+        }
+
+        /*
+         * Now make sure that printf() is NOT marked as mispelt. This
+         * obviously replies on tight coupling between the Markup
+         * Common.FUNCTION and TextTag Format.function.
+         */
+
+        start = pointer.copy();
+        finish = pointer.copy();
+        finish.forwardChars(9);
+
+        word = buffer.getText(start, finish, true);
+
+        assertEquals("dsmthng()", word);
+
+        for (i = 0; i < 9; i++) {
+            tags = pointer.getTags();
+            assertEquals(1, tags.length);
+            tag = tags[0];
+            assertSame(Format.function, tag);
+            pointer.forwardChar();
+        }
+
+        pointer.forwardChar();
+
+        start = pointer.copy();
+        finish = pointer.copy();
+        finish.forwardChars(5);
+
+        word = buffer.getText(start, finish, true);
+        assertEquals("speek", word);
+
+        for (i = 0; i < 5; i++) {
+            tags = pointer.getTags();
+            assertEquals(1, tags.length);
+            tag = tags[0];
+            assertSame(Format.spelling, tag);
+            pointer.forwardChar();
+        }
+
+        assertFalse(pointer.isEnd());
+        assertEquals('.', pointer.getChar());
+        pointer.forwardChar();
         assertTrue(pointer.isEnd());
     }
 }
