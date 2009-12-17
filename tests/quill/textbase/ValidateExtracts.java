4fd9c9b8f9bf5add0f0123abc58821291393b56f
Andrew Cowie <andrew@operationaldynamics.com>
2009-12-17 17:13:45 +1100

    Fix extractParagraphs() to not contain nulls
    Scan for nulls and return an empty Node (for blank lines).
    
    Introduce an EMPTY constant on Node with a no-arg constructor
    representing an empty tree. For other usages, a null Extract still
    means empty.

--- tests/quill/textbase/ValidateExtracts.java
+++ tests/quill/textbase/ValidateExtracts.java
@@ -244,7 +244,6 @@ public class ValidateExtracts extends TestCase
         TextChain text;
         Extract[] lines;
         Extract extract;
-        Span span;
 
         text = new TextChain();
         text.append(createSpan("Pro𝑛to\nSurprise", null));
