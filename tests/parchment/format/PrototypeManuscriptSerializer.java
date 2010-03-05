2def18a6147355c85b9bbcbeda7975497208260d
Andrew Cowie <andrew@operationaldynamics.com>
2010-03-05 18:31:06 +1100

    Add attributes to prototype

--- tests/parchment/format/PrototypeManuscriptSerializer.java
+++ tests/parchment/format/PrototypeManuscriptSerializer.java
@@ -20,6 +20,7 @@ package parchment.format;
 
 import java.io.IOException;
 
+import nu.xom.Attribute;
 import nu.xom.Document;
 import nu.xom.Element;
 import nu.xom.Serializer;
@@ -37,6 +38,7 @@ public class PrototypeManuscriptSerializer
         final Element manuscript, content, presentation;
         final Element renderer, paper, margins, fonts;
         Element chapter;
+        Attribute attribute;
         final Serializer serializer;
 
         manuscript = new ParchmentElement("manuscript");
@@ -45,24 +47,55 @@ public class PrototypeManuscriptSerializer
         presentation = new ParchmentElement("presentation");
 
         chapter = new ParchmentElement("chapter");
+        attribute = new Attribute("src", "Introduction.xml");
+        chapter.addAttribute(attribute);
         content.appendChild(chapter);
+
         chapter = new ParchmentElement("chapter");
+        attribute = new Attribute("src", "Final Everywhere.xml");
+        chapter.addAttribute(attribute);
         content.appendChild(chapter);
         manuscript.appendChild(content);
 
         renderer = new ParchmentElement("renderer");
-        paper = new ParchmentElement("paper");
-        margins = new ParchmentElement("margins");
-        fonts = new ParchmentElement("fonts");
+        attribute = new Attribute("class", "com.operationaldynamics.parchment.ReportRenderEngine");
+        renderer.addAttribute(attribute);
         presentation.appendChild(renderer);
+
+        paper = new ParchmentElement("paper");
+        attribute = new Attribute("size", "A4");
+        paper.addAttribute(attribute);
         presentation.appendChild(paper);
+
+        margins = new ParchmentElement("margins");
+        attribute = new Attribute("top", "10.00");
+        margins.addAttribute(attribute);
+        attribute = new Attribute("left", "57.75");
+        margins.addAttribute(attribute);
+        attribute = new Attribute("right", "25.00");
+        margins.addAttribute(attribute);
+        attribute = new Attribute("bottom", "10.00");
+        margins.addAttribute(attribute);
         presentation.appendChild(margins);
+
+        fonts = new ParchmentElement("fonts");
+        attribute = new Attribute("sans", "Liberation Sans, 8.0");
+        fonts.addAttribute(attribute);
+        attribute = new Attribute("serif", "Linux Libertine, 9.0");
+        fonts.addAttribute(attribute);
+        attribute = new Attribute("mono", "Inconsolata, 8.1");
+        fonts.addAttribute(attribute);
+        attribute = new Attribute("sans", "Linux Libertine O C");
+        fonts.addAttribute(attribute);
+
         presentation.appendChild(fonts);
+
         manuscript.appendChild(presentation);
 
         document = new Document(manuscript);
         serializer = new Serializer(System.out);
         serializer.setIndent(2);
+        serializer.setMaxLength(0);
         serializer.write(document);
     }
 }
