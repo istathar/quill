9f96d4abd3d3bbc4c66b83aa2843f3c611192513
Andrew Cowie <andrew@operationaldynamics.com>
2010-06-23 13:04:48 +1000

    Beginning of utility class to save .parchment files

---
+++ src/parchment/format/ManuscriptConverter.java
@@ -0,0 +1,123 @@
+/*
+ * Quill and Parchment, a WYSIWYN document editor and rendering engine. 
+ *
+ * Copyright © 2010 Operational Dynamics Consulting, Pty Ltd
+ *
+ * The code in this file, and the program it is a part of, is made available
+ * to you by its authors as open source software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License version
+ * 2 ("GPL") as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL for more details.
+ *
+ * You should have received a copy of the GPL along with this program. If not,
+ * see http://www.gnu.org/licenses/. The authors of this program may be
+ * contacted through http://research.operationaldynamics.com/projects/quill/.
+ */
+package parchment.format;
+
+import nu.xom.Attribute;
+import quill.textbase.Folio;
+import quill.textbase.Series;
+
+/**
+ * Utility to create a XOM document for a Manuscript and drive serializing it
+ * out.
+ * 
+ * @author Andrew Cowie
+ */
+/*
+ * Take a different approach here; just use the XOM library API as is.
+ */
+class ManuscriptConverter
+{
+    private ManuscriptElement root;
+
+    private ManuscriptElement content;
+
+    private ManuscriptElement presentation;
+
+    public ManuscriptConverter(final Folio folio) {
+        initialStructure();
+        buildContent(folio);
+        buildPresentation();
+    }
+
+    private void initialStructure() {
+        root = new ManuscriptElement("manuscript");
+
+        content = new ManuscriptElement("content");
+        root.appendChild(content);
+
+        presentation = new ManuscriptElement("presentation");
+        root.appendChild(presentation);
+    }
+
+    private void buildContent(final Folio folio) {
+        int i;
+        Series series;
+        Chapter chapter;
+        ManuscriptElement element;
+        Attribute attribute;
+        String filename;
+
+        for (i = 0; i < folio.size(); i++) {
+            series = folio.getSeries(i);
+            chapter = folio.getChapter(i);
+            filename = chapter.getRelative();
+
+            element = new ManuscriptElement("chapter");
+            attribute = new Attribute("src", filename);
+            element.addAttribute(attribute);
+        }
+    }
+
+    // HARDCODE
+    private void buildPresentation() {
+        ManuscriptElement renderer, paper, margins, font;
+        Attribute attribute;
+
+        renderer = new ManuscriptElement("renderer");
+        attribute = new Attribute("class", "com.operationaldynamics.parchment.ReportRenderEngine");
+        renderer.addAttribute(attribute);
+        presentation.appendChild(renderer);
+
+        paper = new ManuscriptElement("paper");
+        attribute = new Attribute("size", "A4");
+        paper.addAttribute(attribute);
+        presentation.appendChild(paper);
+
+        margins = new ManuscriptElement("margins");
+        attribute = new Attribute("top", "10.00");
+        margins.addAttribute(attribute);
+        attribute = new Attribute("left", "57.75");
+        margins.addAttribute(attribute);
+        attribute = new Attribute("right", "25.00");
+        margins.addAttribute(attribute);
+        attribute = new Attribute("bottom", "10.00");
+        margins.addAttribute(attribute);
+        presentation.appendChild(margins);
+
+        font = new ManuscriptElement("font");
+        attribute = new Attribute("sans", "Liberation Sans, 8.0");
+        font.addAttribute(attribute);
+        presentation.appendChild(font);
+
+        font = new ManuscriptElement("font");
+        attribute = new Attribute("serif", "Linux Libertine, 9.0");
+        font.addAttribute(attribute);
+        presentation.appendChild(font);
+
+        font = new ManuscriptElement("font");
+        attribute = new Attribute("mono", "Inconsolata, 8.1");
+        font.addAttribute(attribute);
+        presentation.appendChild(font);
+
+        font = new ManuscriptElement("font");
+        attribute = new Attribute("heading", "Linux Libertine O C");
+        font.addAttribute(attribute);
+        presentation.appendChild(font);
+    }
+}
---
+++ src/parchment/format/ManuscriptElement.java
@@ -0,0 +1,80 @@
+/*
+ * Quill and Parchment, a WYSIWYN document editor and rendering engine. 
+ *
+ * Copyright © 2008-2010 Operational Dynamics Consulting, Pty Ltd
+ *
+ * The code in this file, and the program it is a part of, is made available
+ * to you by its authors as open source software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License version
+ * 2 ("GPL") as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GPL for more details.
+ *
+ * You should have received a copy of the GPL along with this program. If not,
+ * see http://www.gnu.org/licenses/. The authors of this program may be
+ * contacted through http://research.operationaldynamics.com/projects/quill/.
+ */
+package parchment.format;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import nu.xom.Attribute;
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.Serializer;
+
+/**
+ * Internal root class for &lt;manuscript&gt; tags.
+ * 
+ * @author Andrew Cowie
+ */
+class ManuscriptElement extends Element
+{
+    ManuscriptElement(String name) {
+        super(name, "http://namespace.operationaldynamics.com/parchment/0.2");
+    }
+
+    void setValue(String name, String value) {
+        super.addAttribute(new Attribute(name, value));
+    }
+
+    String getValue(String name) {
+        final Attribute a;
+
+        a = super.getAttribute(name);
+
+        return a.getValue();
+    }
+
+    /**
+     * Set the <code>xml:space="preserve"</code> Attribute on an Element.
+     */
+    /*
+     * How nasty is this expression! No kidding we wrap it up.
+     */
+    void setPreserveWhitespace() {
+        super.addAttribute(new Attribute("xml:space", "http://www.w3.org/XML/1998/namespace", "preserve"));
+    }
+
+    /**
+     * Serialize this document to XML.
+     */
+    // move this to ManuscriptConverter?
+    public void toXML(OutputStream out) throws IOException {
+        final Document doc;
+        final Serializer s;
+
+        doc = new Document(this);
+
+        s = new ManuscriptSerializer(out, this);
+        s.write(doc);
+        s.flush();
+    }
+
+    public String toString() {
+        return "<" + getQualifiedName() + ">";
+    }
+}
