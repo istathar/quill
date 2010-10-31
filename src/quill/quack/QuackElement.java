bcd71cf9ccfca8afa25756246866de7246aab3f1
Andrew Cowie <andrew@operationaldynamics.com>
2010-10-31 02:15:18 +0100

    Update all in-tree .parchment documents to include metadata section
    Change XML namespace version to 0.3, and add a <metadata> element to
    each of the .parchment files' <manuscript> elements.

--- src/quill/quack/QuackElement.java
+++ src/quill/quack/QuackElement.java
@@ -34,7 +34,7 @@ import nu.xom.Serializer;
 abstract class QuackElement extends Element
 {
     QuackElement(String name) {
-        super(name, "http://namespace.operationaldynamics.com/parchment/0.2");
+        super(name, "http://namespace.operationaldynamics.com/parchment/0.3");
     }
 
     void add(Tag tag) {
